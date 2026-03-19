1d3b08d741f16c9d9a88eb982c2eb742c14a9d19
RichardZ24 <134828914+RichardZ24@users.noreply.github.com>
2026-03-19 22:10:10 +1100

    feat: add creation endpoint and request validation

--- app/main.py
+++ app/main.py
@@ -1,11 +1,22 @@
 from fastapi import FastAPI
-
+from pydantic import BaseModel
 app = FastAPI()
+
+class TaskCreate(BaseModel):
+    title: str
+
 
 @app.get("/")
 def read_root():
     return{"message": "API running"}
 
+tasks = ["study docker", "build api"]
+
 @app.get("/tasks")
 def get_tasks():
-    return {"tasks":[]}
+    return {"tasks": tasks}
+
+@app.post("/tasks")
+def create_task(task: TaskCreate):
+    tasks.append(task.title)
+    return {"message": "Task added", "tasks": tasks}
