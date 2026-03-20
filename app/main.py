276986b82ee5558ca79aae1321b424f7aad5c0d3
RichardZ24 <134828914+RichardZ24@users.noreply.github.com>
2026-03-20 17:44:49 +1100

    refactor: improve response message consistency

--- app/main.py
+++ app/main.py
@@ -33,8 +33,9 @@ def create_task(task: TaskCreate):
         new_task_id = 1
     else:
         new_task_id = tasks[-1].id + 1
-    tasks.append(Task(title = task.title, id = new_task_id, completed = False))
-    return {"message": "Task added", "tasks": tasks}
+    new_task = Task(title = task.title, id = new_task_id, completed = False)
+    tasks.append(new_task)
+    return {"message": "Task added", "task": new_task}
 
 @app.put("/tasks/{id}")
 def update_task_completion(id: int, task: TaskCompletion):
@@ -46,20 +47,22 @@ def update_task_completion(id: int, task: TaskCompletion):
             continue
 
         if (new_state == stored_task.completed):
-            return {"message" : f"Task is already marked as {'complete' if (new_state) else 'incomplete'}.", "updated task": task}
+            return {"message" : f"Task is already marked as {'complete' if (new_state) else 'incomplete'}.", "updated task": stored_task}
         
         stored_task.completed = new_state
-        return {"message" : f"Task is marked as {'complete' if (new_state) else 'incomplete'}", "updated task": task}
+        return {"message" : f"Task is marked as {'complete' if (new_state) else 'incomplete'}", "updated task": stored_task}
     return {"message" : "Task was not found, please enter a valid task id."}
     
 
 @app.delete("/tasks/{id}")
 def delete_task(id: int):
     # index based for loop for good practise when modifying a looping list. (unecessary for this instance)
+    
     for i, stored_task in enumerate(tasks):
         if (id == stored_task.id):
+            old_task  = tasks[i]
             tasks.pop(i)
-            return {"message" : "Task successfully removed"}
+            return {"message" : "Task successfully removed", "task" : old_task }
     return {"message" : "Task not found"}
 
 
